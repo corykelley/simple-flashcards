16c04e06855d266d6a37b7e15982d2b1fe132fa7
Cory Kelley <john.cory.kelley@gmail.com>
2025-02-26 15:07:51 -0500

    update styling, cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,39 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "media",
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "1440px",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      xxl: "2560px",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "stormy-blue": "#61788C",
+        "misty-sky": "#B8CAD9",
+        "olive-stone": "#736B46",
+        sand: "#D9C7B8",
+        "midnight-black": "#0D0D0D",
+        "pearl-glow": "#F6F3EE",
+      },
+      borderRadius: {
+        sm: "2px",
+        md: "4px",
+        lg: "8px",
+        xl: "12px",
+        xxl: "16px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
